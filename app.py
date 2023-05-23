174deaf3bb9ae46b72e22878dc44f7b56012936e
SMuskannnn <109818186+SMuskannnn@users.noreply.github.com>
2023-05-23 23:57:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-  
+import random
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -10,16 +10,36 @@ def hello():
 
 
 
+
+def chatbot(user_input):
+    # List of random replies
+    replies = [
+        "Hello!",
+        "How are you?",
+        "Tell me more.",
+        "That's interesting.",
+        "I'm not sure.",
+        "What do you think?",
+        "Let's change the topic.",
+        "Goodbye!",
+    ]
+    
+    # Generate a random reply
+    reply = random.choice(replies)
+    
+    return reply
+
+# Example usage
 @app.route("/sms", methods=['POST'])
 
 def sms_reply():
 #    """Respond to incoming calls with a simple text message."""
     # Fetch the message
-    msg = request.form.get('Body')
-
+    user_input = request.form.get('Body')
+    response = chatbot(user_input)
     # Create reply
     resp = MessagingResponse()
-    resp.message("HAHAHA")
+    resp.message(response)
 
     return str(resp)
 
